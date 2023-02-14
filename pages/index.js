1dd536edd56dc470f51087bf459e546a043634d9
Tarun Yadav <tarunyadav9761@gmail.com>
2023-02-14 23:02:14 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -1,123 +1,305 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import EngagementHelper from '../lib/engagementHelper'
+
+const messageCountList = [
+	{
+		count: '1',
+		timeBucket: '2022-10-10T00:00:00.000Z',
+		channelId: '1030088402701385838',
+	},
+	{
+		count: '1',
+		timeBucket: '2022-10-10T00:00:00.000Z',
+		channelId: '1030087559621136424',
+	},
+	{
+		count: '2',
+		timeBucket: '2022-10-10T00:00:00.000Z',
+		channelId: '1030037314241564732',
+	},
+	{
+		count: '1',
+		timeBucket: '2022-10-10T00:00:00.000Z',
+		channelId: '1029808623901233182',
+	},
+	{
+		count: '1',
+		timeBucket: '2022-10-10T00:00:00.000Z',
+		channelId: '1029806377952088074',
+	},
+	{
+		count: '1',
+		timeBucket: '2022-10-10T00:00:00.000Z',
+		channelId: '1029806106744193067',
+	},
+	{
+		count: '13',
+		timeBucket: '2022-10-10T00:00:00.000Z',
+		channelId: '1029805353019375616',
+	},
+	{
+		count: '5',
+		timeBucket: '2022-10-10T00:00:00.000Z',
+		channelId: '825030076239577109',
+	},
+	{
+		count: '1',
+		timeBucket: '2022-10-17T00:00:00.000Z',
+		channelId: '825030076239577109',
+	},
+	{
+		count: '2',
+		timeBucket: '2022-10-24T00:00:00.000Z',
+		channelId: '1034437865909780580',
+	},
+	{
+		count: '4',
+		timeBucket: '2022-10-24T00:00:00.000Z',
+		channelId: '1034434108241219645',
+	},
+	{
+		count: '3',
+		timeBucket: '2022-10-24T00:00:00.000Z',
+		channelId: '825030076239577109',
+	},
+	{
+		count: '1',
+		timeBucket: '2022-10-31T00:00:00.000Z',
+		channelId: '1037057875543076974',
+	},
+]
+
+const channels = [
+	{
+		label: 'Text Channels',
+		value: '825030075794587692',
+		type: 4,
+		guild: '825030075794587691',
+		guildId: '825030075794587691',
+		parentId: null,
+		permissionOverwrites: [],
+		id: '825030075794587692',
+		name: 'Text Channels',
+		rawPosition: 0,
+		createdTimestamp: 1616772898387,
+	},
+	{
+		label: 'Voice Channels',
+		value: '825030076239577108',
+		type: 4,
+		guild: '825030075794587691',
+		guildId: '825030075794587691',
+		parentId: null,
+		permissionOverwrites: [],
+		id: '825030076239577108',
+		name: 'Voice Channels',
+		rawPosition: 0,
+		createdTimestamp: 1616772898493,
+	},
+	{
+		label: 'general',
+		value: '825030076239577109',
+		type: 0,
+		guild: '825030075794587691',
+		guildId: '825030075794587691',
+		parentId: '825030075794587692',
+		permissionOverwrites: [],
+		messages: [],
+		threads: [],
+		nsfw: false,
+		id: '825030076239577109',
+		name: 'general',
+		rawPosition: 0,
+		topic: null,
+		lastMessageId: '1034437862151696414',
+		rateLimitPerUser: 0,
+		createdTimestamp: 1616772898493,
+	},
+	{
+		label: 'General',
+		value: '825030076239577111',
+		type: 2,
+		guild: '825030075794587691',
+		guildId: '825030075794587691',
+		parentId: '825030076239577108',
+		permissionOverwrites: [],
+		messages: [],
+		nsfw: false,
+		id: '825030076239577111',
+		name: 'General',
+		rawPosition: 0,
+		rtcRegion: null,
+		bitrate: 64000,
+		userLimit: 0,
+		videoQualityMode: null,
+		lastMessageId: null,
+		rateLimitPerUser: 0,
+		createdTimestamp: 1616772898493,
+	},
+	{
+		label: 'csgo',
+		value: '825030110088396860',
+		type: 0,
+		guild: '825030075794587691',
+		guildId: '825030075794587691',
+		parentId: '825030075794587692',
+		permissionOverwrites: [],
+		messages: [],
+		threads: [],
+		nsfw: false,
+		id: '825030110088396860',
+		name: 'csgo',
+		rawPosition: 1,
+		topic: 'This is a good place to talk about csgo',
+		lastMessageId: '1019286026776236062',
+		rateLimitPerUser: 0,
+		createdTimestamp: 1616772906563,
+	},
+	{
+		label: 'priv',
+		value: '1000123565594648677',
+		type: 0,
+		guild: '825030075794587691',
+		guildId: '825030075794587691',
+		parentId: '825030075794587692',
+		permissionOverwrites: [
+			'1018105887564509254',
+			'825030075794587691',
+			'1000083013318758461',
+			'1017825278124380280',
+		],
+		messages: [],
+		threads: [],
+		nsfw: false,
+		id: '1000123565594648677',
+		name: 'priv',
+		rawPosition: 2,
+		topic: null,
+		lastMessageId: null,
+		rateLimitPerUser: 0,
+		createdTimestamp: 1658518439435,
+	},
+	{
+		label: 'another',
+		value: '1000124143808827412',
+		type: 0,
+		guild: '825030075794587691',
+		guildId: '825030075794587691',
+		parentId: '825030075794587692',
+		permissionOverwrites: ['1000083013318758461', '825030075794587691'],
+		messages: [],
+		threads: [],
+		nsfw: false,
+		id: '1000124143808827412',
+		name: 'another',
+		rawPosition: 3,
+		topic: null,
+		lastMessageId: null,
+		rateLimitPerUser: 0,
+		createdTimestamp: 1658518577292,
+	},
+	{
+		label: 'aa',
+		value: '1000124520604115125',
+		type: 0,
+		guild: '825030075794587691',
+		guildId: '825030075794587691',
+		parentId: '825030075794587692',
+		permissionOverwrites: ['825030075794587691', '1017825278124380280'],
+		messages: [],
+		threads: [],
+		nsfw: false,
+		id: '1000124520604115125',
+		name: 'aa',
+		rawPosition: 4,
+		topic: null,
+		lastMessageId: null,
+		rateLimitPerUser: 0,
+		createdTimestamp: 1658518667127,
+	},
+	{
+		label: 'ss',
+		value: '1000125215508017323',
+		type: 0,
+		guild: '825030075794587691',
+		guildId: '825030075794587691',
+		parentId: '825030075794587692',
+		permissionOverwrites: [
+			'1000083151047102514',
+			'825030075794587691',
+			'1018105887564509254',
+		],
+		messages: [],
+		threads: [],
+		nsfw: false,
+		id: '1000125215508017323',
+		name: 'ss',
+		rawPosition: 7,
+		topic: null,
+		lastMessageId: '1000143011751153755',
+		rateLimitPerUser: 0,
+		createdTimestamp: 1658518832805,
+	},
+	{
+		label: 'collabland-config',
+		value: '1001114706238521374',
+		type: 0,
+		guild: '825030075794587691',
+		guildId: '825030075794587691',
+		parentId: null,
+		permissionOverwrites: ['825030075794587691', '1001114703717736489'],
+		messages: [],
+		threads: [],
+		nsfw: false,
+		id: '1001114706238521374',
+		name: 'collabland-config',
+		rawPosition: 8,
+		topic: null,
+		lastMessageId: '1013123227733725345',
+		rateLimitPerUser: 2,
+		createdTimestamp: 1658754745779,
+	},
+	{
+		label: 'collabland-join',
+		value: '1001114709375844382',
+		type: 0,
+		guild: '825030075794587691',
+		guildId: '825030075794587691',
+		parentId: null,
+		permissionOverwrites: ['1001114703717736489', '825030075794587691'],
+		messages: [],
+		threads: [],
+		nsfw: false,
+		id: '1001114709375844382',
+		name: 'collabland-join',
+		rawPosition: 9,
+		topic: null,
+		lastMessageId: '1001151321140904056',
+		rateLimitPerUser: 2,
+		createdTimestamp: 1658754746527,
+	},
+]
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-    </>
-  )
+	const options = EngagementHelper.engagementMessageOverTimeChartOptions(
+		messageCountList,
+		channels
+	)
+	return (
+		<>
+			<Head>
+				<title>Assignment</title>
+				<meta name="description" content="Generated by create next app" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main className={styles.main}>
+				<HighchartsReact highcharts={Highcharts} options={options} />
+			</main>
+		</>
+	)
 }
